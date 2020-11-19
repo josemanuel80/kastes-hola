671d078ce93377b87eb43d1091fa328ad9bb38d3
josemanuel80 <castellanogonzalezjosemanuel@gmail.com>
2020-11-19 13:57:11 +0100

    links added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola</h1>
+        {/* <h1>Hola</h1> */}
         <p1>Kastes</p1>
         {/* <input className="name" placeholder="Buscar"></input> */}
         <a href="https://josemanuel80.github.io/the-game/">The game</a>
         <a href="https://distracted-bell-331846.netlify.app/">Playas</a>
+        <a href="https://www.google.es/webhp?tab=mw&pli=1">Google</a>
       </header>
     </div>
   );
